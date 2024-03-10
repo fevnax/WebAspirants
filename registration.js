71279c5e8e30c2916a1050efa5199a723a7d6707
fevnax <haru54709@gmail.com>
2024-03-10 21:06:57 +0530

    Added Files

---
+++ registration.js
@@ -0,0 +1,5 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Registration successful!");
+     window.location.href = "home.html";
+});
